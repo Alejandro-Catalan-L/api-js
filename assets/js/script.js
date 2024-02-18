214a9c31a0d3a2ade1da564cefeb6d382430924c
Alejandro.Catalan <alejandro.empresas@hotmail.com>
2024-02-18 18:06:55 -0300

    contenido final

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,70 +1,115 @@
-const btnAgregar = document.getElementById("btnAgregar");
-const inputTarea = document.getElementById("inputTarea");
-const spanTotal = document.getElementById("spanTotal"); 
-const spanRealizadas = document.getElementById("spanRealizadas"); 
-const divList = document.getElementById("divList");
+const btnConvertir = document.getElementById('btnConvertir');
+const EntradaPCLP = document.getElementById('EntradaPCLP');
+const seleccionmoneda = document.getElementById('seleccionmoneda');
+const spanTotal = document.getElementById('spanTotal');
+const ctx = document.getElementById('myChart');
+const apiUrl = 'https://mindicador.cl/api/';
+let myChart;
 
-let contadorID = 4;
-let tareas = [
-    { id: 1, descripcion: "Hacer el aseo de la casa", realizada: true },
-    { id: 2, descripcion: "Hacer deportes", realizada: false },
-    { id: 3, descripcion: "Hacer el baño", realizada: false }
-];
+agregarOpcionDesdeAPI();
+btnConvertir.addEventListener('click', convertir)
 
-    let agregar = () => {
-        if(inputTarea.value != ''){
-            tareas.push({
-                id: contadorID, descripcion: inputTarea.value , realizada: false
-            });
-            contadorID++
-            inputTarea.value = "";
-            list();
-        }else{
-            alert("Debes describir una tarea");
-            inputTarea.value = "";
+
+async function convertir(){
+    if(EntradaPCLP.value != ''){
+        const valorMoneda = await buscarMoneda(seleccionmoneda.value);
+        let valorCovertido = (EntradaPCLP.value / valorMoneda).toFixed(1);
+        spanTotal.innerHTML = `Resultado: $${valorCovertido}`;
+    }else{
+        alert("Debes ingresar un monto a convertir");
+        EntradaPCLP.value = "";
+    }
+}
+
+async function buscarMoneda(moneda) {
+    try {
+        const resConvertir = await fetch(`${apiUrl}${moneda}`);
+        
+        if (!resConvertir.ok) {
+            throw new Error(`Error al obtener datos de la API: ${resConvertir.statusText}`);
+        }
+        
+        const dataConvertir = await resConvertir.json();
+        const { serie } = dataConvertir;
+        const grafico = dataGrafico(serie.slice(0, 10).reverse());
+
+        if (myChart) {
+            myChart.data.labels = grafico.fechaGrafico;
+            myChart.data.datasets = grafico.infoGrafico.map(({ label, borderColor, valorGrafico }) => ({
+                label,
+                borderColor,
+                data: valorGrafico
+            }));
+            myChart.update();
+        } else {
+            renderGrafico(grafico);
+        }
+
+        const [{ valor: valorMoneda }] = serie;
+        return valorMoneda;
+
+    } catch (error) {
+        alert(`Error al obtener datos de la API: buscarMoneda: ${error}`);
+    }
+}
+
+async function agregarOpcionDesdeAPI() {
+    try {
+
+        const res = await fetch(apiUrl);
+        const data = await res.json();
+
+        for (const [codigo, info] of Object.entries(data)) {
+            if (codigo && info.nombre) {
+                const option = document.createElement('option');
+                option.value = codigo;
+                option.textContent = `${info.nombre}`;
+                seleccionmoneda.appendChild(option);
+            }
+        }
+    } catch (error) {
+        alert('Error al obtener datos de la API: agregarOpcionDesdeAPI', error);
+    }
+}
+
+function renderGrafico(grafico) {
+    const configurar = {
+        type: "line",
+        data: {
+            labels: grafico.fechaGrafico,
+            datasets: grafico.infoGrafico.map(({ label, borderColor, valorGrafico }) => ({
+                label,
+                borderColor,
+                data: valorGrafico
+            }))
         }
     };
 
-    let list = () => {
-        let tablaHTML = '<table>';
-        tablaHTML += `
-            <tr>
-                <th>ID</th>
-                <th>Tareas</th>
-            </tr>`;
-        tareas.forEach(tarea => {
-        tablaHTML += `
-            <tr>
-                <td>${tarea.id}</td>
-                <td>${tarea.descripcion}</td>
-                <td><input type="checkbox" ${tarea.realizada ? 'checked' : ''} data-id="${tarea.id}" onchange="actualizarCheck(event)"></td>
-                <td><button onclick="borrar(${tarea.id})">x</button></td>
-            </tr>`;
-        });
-        tablaHTML += '</table>';
-        spanTotal.innerHTML = tareas.length;
-        checkMarcados();
-        divList.innerHTML = tablaHTML;
+    if (myChart) {
+        myChart.destroy();
     }
+    ctx.style.backgroundColor = "whitesmoke";
+    myChart = new Chart(ctx, configurar);
+}
 
-    function borrar(id) {
-        tareas.splice(tareas.findIndex(tarea => tarea.id === id), 1);
-        list();
-    }
+function dataGrafico(serie){
+    const fechaGrafico = serie.map(({fecha}) => formatoFecha(fecha));
+    const valorGrafico = serie.map(({valor}) => valor);
+    const infoGrafico = [
+        {
+            label: "Historial",
+            borderColor: "rgb(255, 99, 132)",
+            valorGrafico
+        },
+    ];
+    return {fechaGrafico, infoGrafico};
+}
 
-    function actualizarCheck(e) {
-        const id = parseInt(e.target.dataset.id);
-        const tarea = tareas.find(tarea => tarea.id === id);
-        tarea.realizada = e.target.checked;
-        checkMarcados();
-    }
 
-    function checkMarcados() {
-        let contador = 0;
-        tareas.forEach(tarea => {
-                if (tarea.realizada) {contador++;}
-            });
-        return spanRealizadas.innerHTML = contador;
-    }
-
-btnAgregar.addEventListener("click", agregar);
+function formatoFecha(date){
+    date = new Date(date);
+    const year = date.getFullYear();
+    const month = date.getMonth();
+    const day = date.getDate();
+    return `${year}-${month}-${day}`;
+}
